d2c97d0f8c1509011527a913782a0088af57d48b
Vaibhav <vaibhav.sharma3100@gmail.com>
2025-01-06 23:35:53 +0530

    performRun function

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import OpenAI from "openai";
 import { createAssistant } from "./openai/createAssistant";
 import { createThread } from "./openai/createThread";
 import { createRun } from "./openai/createRun";
+import { performRun } from "./openai/performRun";
 
 async function main(){
     const client = new OpenAI();
@@ -10,8 +11,9 @@ async function main(){
     const assistant = await createAssistant(client);
     const thread = await createThread(client, message);
     const run = await createRun(client, thread, assistant.id);
+    const result  = await performRun(run, client, thread);
 
 
-    console.log('Hello World');
+    console.log(result);
 }
 main();
